bf1d3b6da4dc113b4610eae045360b189dc494b4
Nicholas Neumann-Chun <nicholas.babelthuap@gmail.com>
2015-12-06 18:06:57 -0800

    Restructure so that List is the parent of all other components

--- public/script.js
+++ public/script.js
@@ -2,24 +2,44 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
-    constructor(props){
-    super(props)
-    this.state = { sortedBookmarkList:[] } 
-  }
-
   render() {
     return (
       <div>
         <h1>Bookmarks</h1>
         <hr />
-        <SortBy sortText={this.state.sortText} />
-        <List data={bookmarkdata} />
-        <Form />
+        <List />
       </div>
     );
   }
 }
 
+class List extends React.Component {
+  constructor(props){
+    super(props);
+    console.log("props", this.props)
+
+    this.state = { data: [
+      {"createdAt": "1449445818815", "title": "Kumano Kodo", "url":"http://www.tb-kumano.jp/en/kumano-kodo/#overview" },
+      {"createdAt": "1449445720407", "title": "Ise Jingu", "url":"http://www.isejingu.or.jp/english/" },
+      {"createdAt": "1449445829857", "title": "Takachiho", "url":"http://www.jnto.go.jp/eng/location/regional/miyazaki/takachiho.html" }
+    ] };
+  }
+
+  render() {
+    return (
+      <div>
+        <SortBy sortText={this.state.sortText} />
+        <ul>
+          { this.state.data.map((bookmark, index)=> {
+            return ( <li key={index}><a href={bookmark.url}>{bookmark.title}</a></li> ) })
+          }
+        </ul>
+        <div>Total: {this.state.data.length}</div>
+        <Form />
+      </div>
+    );
+  }
+}
 
 class SortBy extends React.Component {
   sortList(e) {
@@ -27,54 +47,28 @@ class SortBy extends React.Component {
     console.log("sortlist", e.target.value)
     
   }
-
-  // sortText:
-
+  // sortText() {}
   render() {
     return (
       <div>
         <span>Sort By: </span>
-        <button value="date" onClick={this.sortList.bind(this)} sortText={this.props.sortText} >Date Added</button>
-        <button value="title" onClick={this.sortList.bind(this)}  sortText={this.props.sortText} >Title</button>
+        <button className="btn btn-default" onClick={this.sortList.bind(this)} sortText={this.props.sortText} >Date Added</button>
+        <button className="btn btn-default" onClick={this.sortList.bind(this)} sortText={this.props.sortText} >Title</button>
       </div>
     );
   }
 }
 
-let bookmarkdata = [
-  {"createdAt": "1449445818815", "title": "Kumano Kodo", "url":"http://www.tb-kumano.jp/en/kumano-kodo/#overview" },
-  {"createdAt": "1449445720407", "title": "Ise Jingu", "url":"http://www.isejingu.or.jp/english/" },
-  {"createdAt": "1449445829857", "title": "Takachiho", "url":"http://www.jnto.go.jp/eng/location/regional/miyazaki/takachiho.html" }
-]
-
-class List extends React.Component {
-    constructor(props){
-      super(props);
-      console.log("props", this.props)
-    }
-
-    render() {
-        return (
-            <div>
-              { this.props.data.map((obj, index)=> {
-                return ( <ul key={index}><a href={obj.url}> {obj.title} </a></ul> ) })
-              }
-              <div>total</div>
-            </div>
-        );
-    }
-}
-
 class Form extends React.Component {
-    render() {
-        return (
-          <form>
-            <input type="text" placeholder="Title" required />
-            <input type="url" placeholder="URL" required />
-            <button type="submit">Add Bookmark</button>
-          </form>
-        );
-    }
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Title" required />
+        <input type="url" placeholder="URL" required />
+        <button className="btn btn-default" type="submit">Add Bookmark</button>
+      </form>
+    );
+  }
 }
 
 
